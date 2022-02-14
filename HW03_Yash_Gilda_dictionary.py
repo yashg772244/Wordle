54e3c1a5b98cc089ba53f7d978b416c8c9a5fb67
Yash Gilda <yashgilda@Yashs-MacBook-Pro-2.local>
2022-02-14 16:47:54 -0500

    Updated Code

--- HW03_Yash_Gilda_dictionary.py
+++ HW03_Yash_Gilda_dictionary.py
@@ -4,11 +4,12 @@ def remove(string):
     return string.replace("\n", "")
 list = []
 #Imports the dictionary
-d = open("/Users/yashgilda/desktopp/MSCS/Sem_2/SSW 810 /HW_03/words.txt", "r")
+d = open("words.txt", "r")
 for x in d:
     x = remove(x)
     if len(x) == 5:
         list.append(x)
 #Chooses the wordle
-wordle = random.choice(list)
+def wordleAns():
+    return random.choice(list)
 
