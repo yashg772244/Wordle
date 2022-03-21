c5a3933484ba48478c933ad8a76f506e73bf2865
yashg772244 <95985335+yashg772244@users.noreply.github.com>
2022-03-21 16:27:33 -0400

    Added __init__ and __str__

--- HW03_Yash_Gilda_ui.py
+++ HW03_Yash_Gilda_ui.py
@@ -1,116 +1,129 @@
 import HW03_Yash_Gilda_dictionary as dic
-import HW03_Yash_Gilda_wordle as wrd
+from HW03_Yash_Gilda_wordle import Wordle as wrd
 import HW03_Yash_Gilda_logger as logger
 # Begin
-def main():
-    print("Welcome to Wordle")
+class UI:
     gamesPlayed = 0
     gamesWon = 0
-    guessDistribution = [0,0,0,0,0,0]
-    while(True):
-        gamesPlayed += 1
-        counter = 0
-        wordle = dic.wordleAns()
-        print(wordle)
-        #print(dic.myList)
-        attempt_list = []
-        while counter < 6:
-            boolean = False
-            try:
-                user_input = input("Enter the word: ")
-                user_input = user_input.lower()
-            except:
-                print("Error in taking user input")
-            # Condition to check length of user input
-            if (exit_game(user_input)):
-                quit()
-            if check_len(user_input):
-                print("The word must consist of 5 alphabets only")
-                counter = counter - 1
-            # Condition to check whether the user input contains only alphabets or not
-            elif (check_char(user_input)):
-                print("The word must consist of alphabets only")
-                counter = counter - 1
-            else:
-                # Condition to check if user input is in the dictionary provided
-                if check_dic(user_input):
-                    print("Word is not in the Dictionary")
+    guessDistribution = []
+    def __init__(self):
+        self.wrd = wrd()
+        self.dic = dic.Word_Dictionary()
+        self.gamesPlayed = 0
+        self.gamesWon = 0
+        self.guessDistribution = [0,0,0,0,0,0]
+
+    def __str__(self) -> str:
+        return f"UI(wrd:{str(self.wrd)},dic:{str(self.dic)},gamesPlayed:{str(self.gamesPlayed)},gamesWon:{str(self.gamesWon)},guessDistribution:{str(self.guessDistribution)})"
+    
+    def main(self):
+        print("Welcome to Wordle")
+        self.dic.filter_dic()
+        while(True):
+            self.gamesPlayed += 1
+            counter = 0
+            wordle = self.dic.wordleAns()
+            print(wordle)
+            #print(self.dic.myList)
+            attempt_list = []
+            while counter < 6:
+                boolean = False
+                try:
+                    user_input = input("Enter the word: ")
+                    user_input = user_input.lower()
+                except:
+                    print("Error in taking user input")
+                # Condition to check length of user input
+                if (self.exit_game(user_input)):
+                    quit()
+                if self.check_len(user_input):
+                    print("The word must consist of 5 alphabets only")
                     counter = counter - 1
-                    attempt = 5 - counter
-                    print("Remaining Attempts: ", attempt)
-                elif user_input in dic.myList:
-                    if user_input not in attempt_list:
-                        attempt_list.append(user_input)
-                        # Condition to check if user input matches the wordle
-                        if user_input == wordle:
-                            gamesWon += 1
-                            guessDistribution[counter] += 1
-                            print("Congratulations! You have solved the Wordle!")
-                            break
-                        # Condition to call the compare_wordle function
-                        else:
-                            wrd.compare_wordle(user_input, wordle)
-                            attempt = 5 - counter
-                            print("Remaining Attempts: ", attempt)
-                    else:
-                        # Condition to reset counter if the word is used previously
-                        print("You have attempted this word earlier. Please try another word.")
+                # Condition to check whether the user input contains only alphabets or not
+                elif (self.check_char(user_input)):
+                    print("The word must consist of alphabets only")
+                    counter = counter - 1
+                else:
+                    # Condition to check if user input is in the dictionary provided
+                    if self.check_dic(user_input):
+                        print("Word is not in the Dictionary")
                         counter = counter - 1
                         attempt = 5 - counter
                         print("Remaining Attempts: ", attempt)
-            counter += 1
-        if counter > 5:
-            print("Sorry, but you have failed to solve the wordle!!! Please try again!!! ")
-        print("GAME STATISTICS")
-        print(f"Number of games played {gamesPlayed}")
-        print(f"Win percentage {(gamesWon*100/gamesPlayed): .2f}")
-        print(f"Guessed in 1st attempt: {guessDistribution[0]}")
-        print(f"Guessed in 2nd attempt: {guessDistribution[1]}")
-        print(f"Guessed in 3rd attempt: {guessDistribution[2]}")
-        print(f"Guessed in 4th attempt: {guessDistribution[3]}")
-        print(f"Guessed in 5th attempt: {guessDistribution[4]}")
-        print(f"Guessed in 6th attempt: {guessDistribution[5]}")
-        logger.game_logger(wordle, attempt_list, gamesPlayed, gamesWon, guessDistribution)
+                    elif user_input in self.dic.myList:
+                        if user_input not in attempt_list:
+                            attempt_list.append(user_input)
+                            # Condition to check if user input matches the wordle
+                            if user_input == wordle:
+                                self.gamesWon += 1
+                                self.guessDistribution[counter] += 1
+                                print("Congratulations! You have solved the Wordle!")
+                                break
+                            # Condition to call the compare_wordle function
+                            else:
+                                self.wrd.compare_wordle(user_input, wordle)
+                                attempt = 5 - counter
+                                print("Remaining Attempts: ", attempt)
+                        else:
+                            # Condition to reset counter if the word is used previously
+                            print("You have attempted this word earlier. Please try another word.")
+                            counter = counter - 1
+                            attempt = 5 - counter
+                            print("Remaining Attempts: ", attempt)
+                counter += 1
+            if counter > 5:
+                print("Sorry, but you have failed to solve the wordle!!! Please try again!!! ")
+            print("GAME STATISTICS")
+            print(f"Number of games played {self.gamesPlayed}")
+            print(f"Win percentage {(self.gamesWon*100/self.gamesPlayed): .2f}")
+            print(f"Guessed in 1st attempt: {self.guessDistribution[0]}")
+            print(f"Guessed in 2nd attempt: {self.guessDistribution[1]}")
+            print(f"Guessed in 3rd attempt: {self.guessDistribution[2]}")
+            print(f"Guessed in 4th attempt: {self.guessDistribution[3]}")
+            print(f"Guessed in 5th attempt: {self.guessDistribution[4]}")
+            print(f"Guessed in 6th attempt: {self.guessDistribution[5]}")
+            logger.game_logger(wordle, attempt_list, self.gamesPlayed, self.gamesWon, self.guessDistribution)
 
-def exit_game(a):
-    # Condition to check length of user input
-    try:
-        if(len(a)== 0):
-            return True
-        else:
-            return False
-    except:
-        print("Error Exiting Game")
+    def exit_game(self, a):
+        # Condition to check length of user input
+        try:
+            if(len(a)== 0):
+                return True
+            else:
+                return False
+        except:
+            print("Error Exiting Game")
 
-def check_char(a):
-    # Condition to check whether the user input contains only alphabets or not
-    try:
-        if(a.isalpha()):
-            return False
-        else:
-            return True
-    except:
-        print("Error in checking character")
+    def check_char(self, a):
+        # Condition to check whether the user input contains only alphabets or not
+        try:
+            if(a.isalpha()):
+                return False
+            else:
+                return True
+        except:
+            print("Error in checking character")
 
-def check_dic(a):
-    try:
-        if(a not in dic.myList):
-            return True
-        else:
-            return False
-    except:
-        print("")
+    def check_dic(self, a):
+        try:
+            if(a not in self.dic.myList):
+                return True
+            else:
+                return False
+        except:
+            print("")
 
-def check_len(a):
-    try:
-        if(len(a) != 5):
-            return True
-        else:
-            return False
-    except:
-        print("Error in checking length of character")
+    def check_len(self, a):
+        try:
+            if(len(a) != 5):
+                return True
+            else:
+                return False
+        except:
+            print("Error in checking length of character")
 
 if __name__ == "__main__":
-    main()
+    ui = UI()
+    ui.main()
 
 
--- HW_07_Yash_Gilda_letterFrequency.py
+++ HW_07_Yash_Gilda_letterFrequency.py
@@ -1,88 +1,97 @@
 import csv
 import string
-wrd_occurence = dict( (key, [0,0,0,0,0]) for key in string.ascii_lowercase )
-myList = []
-#Calculates the Letter Likelihood
-def letter_likelihood():
-    r = open("word_list.txt", "r")
-    files = r.read()
-    dictList = files.split("\n")
-    myList = list(dictList)
-    r.close()
-    for wrd in myList:
-        count = 0
-        for letter in wrd:
-            wrd_occurence[letter][count] += 1
-            count+=1
+
+class Letter_Frequency:
+    wrd_occurence = dict()
+    myList = []
+    def __init__(self):
+        self.myList = []
+        self.wrd_occurence = dict( (key, [0,0,0,0,0]) for key in string.ascii_lowercase )
     
-    for letter in string.ascii_lowercase:
-        flag = 0
-        for wrd in myList:
+    def __str__(self) -> str:
+        return f"Letter_Frequency(myList:{str(self.myList)},wrd_occurence:{str(self.wrd_occurence)})"
+    
+    #Calculates the Letter Likelihood
+    def letter_likelihood(self):
+        r = open("word_list.txt", "r")
+        files = r.read()
+        dictList = files.split("\n")
+        self.myList = list(dictList)
+        r.close()
+        for wrd in self.myList:
+            count = 0
+            for letter in wrd:
+                self.wrd_occurence[letter][count] += 1
+                count+=1
+        
+        for letter in string.ascii_lowercase:
+            flag = 0
+            for wrd in self.myList:
+                flag += 1
+            for i in range(5):
+                self.wrd_occurence[letter][i] = round((self.wrd_occurence[letter][i] / flag),4)
+
+        f = open("letterFrequency.csv", 'w')
+        for letter in string.ascii_lowercase:
+            f.write(f"{letter}, {self.wrd_occurence[letter][0]}, {self.wrd_occurence[letter][1]}, {self.wrd_occurence[letter][2]}, {self.wrd_occurence[letter][3]}, {self.wrd_occurence[letter][4]}\n")
+
+        return self.wrd_occurence
+
+    #Converting the list to Tuple
+    def list_To_tuple(self, diction):
+        for key in diction.keys():
+            diction[key] = tuple(diction[key])
+        return diction
+
+    #Convert the Statistics into a Dictionary of Tuples
+    def statsTodicTuples(self):
+        file = open("letterFrequency.csv")
+        csvreader = csv.reader(file)
+        wrd_occurence_csv = {}
+        for row in csvreader:
+            wrd_occurence_csv[row[0]] = (row[1], row[2], row[3], row[4], row[5])
+        #print(word_occurence_csv)
+        file.close()
+        return wrd_occurence_csv
+
+    #Calculates the Occurence Likelihood      
+    def occ_likelihood(self):
+        occ_like = {}
+        if self.check_len(wrd):
+            for wrd in self.myList:
+                occurence_likelihood = float(self.wrd_occurence[wrd[0]][0]) * float(self.wrd_occurence[wrd[1]][1]) * float(self.wrd_occurence[wrd[2]][2]) * float(self.wrd_occurence[wrd[3]][3]) * float(self.wrd_occurence[wrd[4]][4])
+                if self.check_likelihood_range(occurence_likelihood):
+                    occ_like[wrd] = occurence_likelihood
+
+        sorted_list = sorted(occ_like.items(), key=lambda x:x[1])
+        sorted_list.reverse()
+        f = open("wordRank.csv", 'w')
+        f.write("Rank, Word, Likelihood \n")
+        flag = 1
+        for wrd in sorted_list:
+            f.write(f"{flag}, {wrd[0]}, {wrd[1]}\n")
             flag += 1
-        for i in range(5):
-            wrd_occurence[letter][i] = round((wrd_occurence[letter][i] / flag),4)
+        f.close()
+        return sorted_list
 
-    f = open("letterFrequency.csv", 'w')
-    for letter in string.ascii_lowercase:
-        f.write(f"{letter}, {wrd_occurence[letter][0]}, {wrd_occurence[letter][1]}, {wrd_occurence[letter][2]}, {wrd_occurence[letter][3]}, {wrd_occurence[letter][4]}\n")
+    #Checks the length of the word
+    def check_len(self, wrd):
+        if len(wrd) == 5:
+            return True
+        else:
+            return False
 
-    return wrd_occurence
-
-#Converting the list to Tuple
-def list_To_tuple(diction):
-    for key in diction.keys():
-        diction[key] = tuple(diction[key])
-    return diction
-
-#Convert the Statistics into a Dictionary of Tuples
-def statsTodicTuples():
-    file = open("letterFrequency.csv")
-    csvreader = csv.reader(file)
-    wrd_occurence_csv = {}
-    for row in csvreader:
-        wrd_occurence_csv[row[0]] = (row[1], row[2], row[3], row[4], row[5])
-    #print(word_occurence_csv)
-    file.close()
-    return wrd_occurence_csv
-
-#Calculates the Occurence Likelihood      
-def occ_likelihood():
-    occ_like = {}
-    if check_len(wrd):
-        for wrd in myList:
-            occurence_likelihood = float(wrd_occurence[wrd[0]][0]) * float(wrd_occurence[wrd[1]][1]) * float(wrd_occurence[wrd[2]][2]) * float(wrd_occurence[wrd[3]][3]) * float(wrd_occurence[wrd[4]][4])
-            if check_likelihood_range(occurence_likelihood):
-                occ_like[wrd] = occurence_likelihood
-
-    sorted_list = sorted(occ_like.items(), key=lambda x:x[1])
-    sorted_list.reverse()
-    f = open("wordRank.csv", 'w')
-    f.write("Rank, Word, Likelihood \n")
-    flag = 1
-    for wrd in sorted_list:
-        f.write(f"{flag}, {wrd[0]}, {wrd[1]}\n")
-        flag += 1
-    f.close()
-    return sorted_list
-
-#Checks the length of the word
-def check_len(wrd):
-    if len(wrd) == 5:
-        return True
-    else:
-        return False
-
-def check_likelihood_range(likelihood):
-    if likelihood<1:
-        return True
-    else:
-        return False
+    def check_likelihood_range(self, likelihood):
+        if likelihood<1:
+            return True
+        else:
+            return False
 
 
-#print(letter_likelihood())
-#print()
-#print(list_To_tuple(wrd_occurence))
-#print()
-#print(statsTodicTuples())
-#print()
-#print(occ_likelihood())
+    #print(letter_likelihood())
+    #print()
+    #print(list_To_tuple(wrd_occurence))
+    #print()
+    #print(statsTodicTuples())
+    #print()
+    #print(occ_likelihood())
